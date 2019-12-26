f15852a7a90743d749c99fff874adac0eebd4b93
ryu22 <ryu2208@naver.com>
2019-12-26 15:29:34 +0900

    post

---
+++ 단계별/1. 입출력과 사칙연산/1. Hello World.cpp
@@ -0,0 +1,8 @@
+# 2557
+
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Hello World!";
+	return 0;
+}
---
+++ 단계별/1. 입출력과 사칙연산/7. AxB.cpp
@@ -0,0 +1,10 @@
+#10998
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+	return 0;
+}
