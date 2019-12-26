a50d4c6e43a4ca900195e7d058b815a7687d0794
ryu22 <ryu2208@naver.com>
2019-12-26 16:22:23 +0900

    day1

--- 단계별/2. if문/5. 세 수.cpp
+++ 단계별/2. if문/5. 세 수.cpp
@@ -5,11 +5,8 @@ using namespace std;
 int main(){
 	int a, b, c;
 	cin >> a >> b >> c;
-	// b>a>c || c>a>b
 	if((b>=a && a>=c ) || (c>=a && a>=b)) cout << a;
-	// a>b>c || c>b>a
 	else if((a>=b && b>=c) || (c>=b && c>=a)) cout << b;
-	// a>c>b || b>c>a
 	else cout << c;	
 	return 0;
 }
