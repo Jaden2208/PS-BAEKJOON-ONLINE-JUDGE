c40e2a63be0069999a5df3f5b4016ca62cfa3e92
ryu22 <ryu2208@naver.com>
2020-01-08 16:36:06 +0900

    day7

--- 단계별/8. 수학1/3. 벌집.cpp
+++ 단계별/8. 수학1/3. 벌집.cpp
@@ -3,18 +3,35 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int n;
-	cin >> n;
-	int cnt = 1;
-	if(n == 1){
-		cout << cnt;
-		return 0;
-	}
-	--n;
-	while(n >= 1){
-		n -= 6*cnt;
-		++cnt;
-	}
-	cout << cnt;
-	return 0;
+   int num;
+   cin >> num;
+   
+   int total=0;
+   int n =0;
+   while( 1+3*n*(n+1) < num){
+      ++n;
+   }
+   
+   cout << n+1;
+      return 0;
 }
+
+//´ú ÁÁÀº ÄÚµå 
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cin >> n;
+//	int cnt = 1;
+//	if(n == 1){
+//		cout << cnt;
+//		return 0;
+//	}
+//	--n;
+//	while(n >= 1){
+//		n -= 6*cnt;
+//		++cnt;
+//	}
+//	cout << cnt;
+//	return 0;
+//}
--- 단계별/8. 수학1/4. 분수찾기.cpp
+++ 단계별/8. 수학1/4. 분수찾기.cpp
@@ -3,41 +3,56 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int n;
-	cin >> n;
-	int a = 1;
-	int b = 1;
-	
-	if(n==1){
-		cout << a << '/' << b;
-		return 0;
-	}
-	bool AupBdown = false;
-	for(int i=1; i<n; i++){
-		if(a==1){
-			AupBdown = true;
-			if(b%2==1){
-				++b;
-				continue;
-			}
-		}
-		else if(b==1){
-			AupBdown = false;
-			if(a%2==0){
-				++a;
-				continue;
-			}
-		}
-		if(AupBdown){
-			++a;
-			--b;
-		}
-		else{
-			--a;
-			++b;
-		}
-	}
-	cout << a << '/' << b;
-	
-	return 0;
+	int num;
+    cin >> num;
+    int n=0;
+    while( n*(n+1)/2 < num){
+       ++n;
+    }     
+    if(n%2 == 0) cout <<  num - (n*(n-1)/2) << '/' << (n+1) - (num - (n*(n-1)/2));
+    else cout << (n+1) - (num - (n*(n-1)/2))  << '/' << num - (n*(n-1)/2);
+    return 0;
 }
+
+// ´ú ÁÁÀº ÄÚ 
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cin >> n;
+//	int a = 1;
+//	int b = 1;
+//	
+//	if(n==1){
+//		cout << a << '/' << b;
+//		return 0;
+//	}
+//	bool AupBdown = false;
+//	for(int i=1; i<n; i++){
+//		if(a==1){
+//			AupBdown = true;
+//			if(b%2==1){
+//				++b;
+//				continue;
+//			}
+//		}
+//		else if(b==1){
+//			AupBdown = false;
+//			if(a%2==0){
+//				++a;
+//				continue;
+//			}
+//		}
+//		if(AupBdown){
+//			++a;
+//			--b;
+//		}
+//		else{
+//			--a;
+//			++b;
+//		}
+//	}
+//	cout << a << '/' << b;
+//	
+//	return 0;
+//}
