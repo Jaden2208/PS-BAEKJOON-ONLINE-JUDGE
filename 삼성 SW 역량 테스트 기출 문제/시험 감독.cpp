542b781b9c68e7381469f7a71df81b8031a55909
ryu22 <ryu2208@naver.com>
2020-03-23 19:44:23 +0900

    sssw

--- 삼성 SW 역량 테스트 기출 문제/시험 감독.cpp
+++ 삼성 SW 역량 테스트 기출 문제/시험 감독.cpp
@@ -1,3 +1,29 @@
 // 13458
 
-
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cin >> n;
+//	int *arr = new int[n];
+	int arr[1000000];
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
+	}
+	
+	int b, c;
+	cin >> b >> c;
+	
+	long long result = 0;
+	for(int i=0; i<n; i++){
+		int remain = arr[i]-b;
+		++result;
+		if(remain <= 0) continue;
+		result += (remain/c);
+		if(remain%c > 0) ++result;
+	}
+	cout << result;
+	
+	return 0;
+}
