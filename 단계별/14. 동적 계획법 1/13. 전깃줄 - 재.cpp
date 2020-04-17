b868450bba221f2aadce974dbdecc4463f76071b
ryu22 <ryu2208@naver.com>
2020-04-17 14:31:55 +0900

    re

--- 단계별/14. 동적 계획법 1/13. 전깃줄 - 재.cpp
+++ 단계별/14. 동적 계획법 1/13. 전깃줄 - 재.cpp
@@ -1,8 +1,31 @@
 // 2565
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
+	
+	int n;
+	cin >> n;
+	int arr[n][2];
+	int dp[n];
+	for(int i=0; i<n; i++){
+		cin >> arr[i][0] >> arr[i][1];
+		dp[i] = 1;
+	}
+
+	int maxCnt = 0;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<i; j++){
+			if(arr[i][1] > arr[j][1]){ 
+				dp[i] = max(dp[i], 1 + dp[j]);
+			}
+		}
+		maxCnt = max(maxCnt, dp[i]);
+	}
+	
+	cout << n - maxCnt;
+	
 	
 	return 0;
 }
