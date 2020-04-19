7d3b69dcd71cb95d3258b6c39fe25fc62958a0c6
ryu22 <ryu2208@naver.com>
2020-04-19 13:14:37 +0900

    re

--- 단계별/22. DFS와 BFS/6. 토마토.cpp
+++ 단계별/22. DFS와 BFS/6. 토마토.cpp
@@ -1,44 +1,63 @@
-// 7576 할 수 있을 것 같음 패쓰 !
+// 7576
 
 #include <iostream>
 #include <queue>
-#define MAX 1000
+#include <algorithm>
 using namespace std;
 
-typedef pair<int, int> P;
-
-int dx[4] = {1,-1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
-int tomato[MAX][MAX];
 int m, n;
-queue<P> start;
+int box[1000][1000];
+int days = 0;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+queue<pair<int, int>> q;
 
-void bfs(int x, int y){
-	for(int i=0; i<4; i++){
-		int nextX = x + dx[i];
-		int nextY = y + dy[i];
-		if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
-		if(tomato[nextX][nextY] == 0){
-			tomato[nextX][nextY] = 1;
-			start.push(P(nextX, nextY))
+bool allRiped(){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			if(box[i][j] == 0) return false;
 		}
 	}
+	return true;
+}
+
+void bfs(){
+	
+	int xx, yy;
+	
+	while(!q.empty()){
+		xx = q.front().first;
+		yy = q.front().second;
+		q.pop();
+		for(int i=0; i<4; i++){
+			int nextX = xx + dx[i];
+			int nextY = yy + dy[i];
+			
+			if(nextX<0 || nextX>=n || nextY<0 || nextY>=m || box[nextX][nextY] != 0) continue;
+			q.push(pair<int, int>(nextX, nextY));
+			box[nextX][nextY] = box[xx][yy] + 1;
+		}
+	}
+	days = max(days, box[xx][yy] - 1);
+	
 }
 
 int main(){
+	
 	cin >> m >> n;
+	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<m; j++){
-			cin >> tomato[i][j];
-			if(tomato[i][j] == 1) start.push(P(i, j));
+			cin >> box[i][j];
+			if(box[i][j] == 1) q.push(pair<int, int>(i, j));
 		}
 	}
 	
-	while(start.size())){
-		P from = start.front();
-		bfs(from.first, from.second);
-		
-	}
+	bfs();
+	
+	if(!allRiped()) days = -1;
+	
+	cout << days;
 	
 	return 0;
 }
